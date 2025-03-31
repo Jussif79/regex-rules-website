72691b17932c078e6fcc8dbd695a47a9e77afb98
Jussif79 <jussif.79@gmail.com>
2025-03-31 13:31:12 +0200

    Add LanguageSwitcher

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="container mx-auto">
+    <LanguageSwitcher         
+        :currentLanguage="currentLanguage" 
+        @switch-language="switchLanguage"
+        class="mb-8"
+        />
+    <RegexRule />
+  </div>
+  <footer>
+    <p class="text-center text-gray-500 text-sm">
+      &copy; 2025 Regex Rule Generator
+    </p>
+  </footer>
 </template>
+
+<script>
+  import { ref } from 'vue'
+  import LanguageSwitcher from './components/LanguageSwitcher.vue'
+  import RegexRule from './components/RegexRule.vue'
+
+  export default {
+    components: {
+      LanguageSwitcher,
+      RegexRule
+    },
+    setup() {
+      const currentLanguage = ref('de')
+    }
+  }
+</script>
---
+++ src/components/LanguageSwitcher.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex space-x-4">
+        <button @click = "$emit('switch-language', 'de')" class="px-4 py-2 rounded">Deutsch</button>
+        <button @click="$emit('switch-language', 'en')" class="px-4 py-2 rounded" >Englisch</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currentLanguage: {
+            type: String,
+            required: true
+        }
+    },
+    emits: ['switch-language']
+}
+</script>
